377ec67fd4e2baf32997e754e8363482701b2a42
Charles Bentley <cbentley@mail.ru>
2020-12-14 19:14:42 +0100

    fix debug prepro

--- src/pasa/cbentley/core/swing/ctx/SwingCoreCtx.java
+++ src/pasa/cbentley/core/swing/ctx/SwingCoreCtx.java
@@ -7,11 +7,15 @@ import javax.swing.JFrame;
 import pasa.cbentley.core.j2se.ctx.J2seCtx;
 import pasa.cbentley.core.src4.logging.Dctx;
 import pasa.cbentley.core.src5.ctx.C5Ctx;
+//#debug
 import pasa.cbentley.core.swing.stringables.StringableJFrame;
 import pasa.cbentley.core.swing.stringables.SwingCoreDebug;
 
 public class SwingCoreCtx extends J2seCtx {
 
+   private Thread         edt;
+
+   //#debug
    private SwingCoreDebug scd;
 
    public SwingCoreCtx(C5Ctx c5) {
@@ -21,20 +25,13 @@ public class SwingCoreCtx extends J2seCtx {
       scd = new SwingCoreDebug(this);
    }
 
-   public SwingCoreDebug toSCD() {
-      return scd;
-   }
-
-   private Thread edt;
-
-
    /**
     * Call this from outside the AWT thread.
     * @param run
     */
    public void edtJoin(Runnable run) {
       EventQueue.invokeLater(run);
-      
+
       try {
          //invoke this method and wait until it finishes in the EDT thread.
          EventQueue.invokeAndWait(new Runnable() {
@@ -56,14 +53,18 @@ public class SwingCoreCtx extends J2seCtx {
    }
 
    //#mdebug
+   public SwingCoreDebug toSCD() {
+      return scd;
+   }
+
+   public StringableJFrame toStrGetJFrame(JFrame f) {
+      return new StringableJFrame(this, f);
+   }
+
    public void toString(Dctx dc) {
       dc.root(this, SwingCoreCtx.class, "@line5");
       toStringPrivate(dc);
       super.toString(dc.sup());
-   }
-
-   private void toStringPrivate(Dctx dc) {
-
    }
 
    public void toString1Line(Dctx dc) {
@@ -72,8 +73,8 @@ public class SwingCoreCtx extends J2seCtx {
       super.toString1Line(dc.sup1Line());
    }
 
-   public StringableJFrame toStrGetJFrame(JFrame f) {
-      return new StringableJFrame(this, f);
+   private void toStringPrivate(Dctx dc) {
+
    }
    //#enddebug
 
